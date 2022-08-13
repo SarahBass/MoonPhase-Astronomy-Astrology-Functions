7973a3dd2b4bd6fc6a3275ac6fe70de685fd0e2d
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-08-13 04:12:49 -0700

    Update gethoroscope.kt

--- gethoroscope.kt
+++ gethoroscope.kt
@@ -91,77 +91,87 @@ fun handleUpdateTimeMessage() {
                 null
             )
         
-        //Get moonphase: 
-        /*
-        private fun getMoonPhase(): String {
-            val sdf = SimpleDateFormat("EEE")
-            val sdf1 = SimpleDateFormat("EEEE")
-            val sdf2 = SimpleDateFormat("MMMM")
-            val sdf3 = SimpleDateFormat("d")
-            val sdf4 = SimpleDateFormat("yyyy")
-            val sdf5 = SimpleDateFormat("MMMM d yyyy")
+         private fun getMoonPhase(): String {
             val d = Date()
-            val dayOfTheWeek: String = sdf.format(d)
-            val dayOfTheWeekLong: String = sdf1.format(d)
-            val monthOfYear: String = sdf2.format(d)
-            val dayOfMonth: String = sdf3.format(d)
-            val year4digits: String = sdf4.format(d)
-            val fullDateSpaces: String = sdf5.format(d)
-            val moonPercent : Double = 0.5
+            val sdf0 = SimpleDateFormat("yyyy MMMM")
+            val sdf1 = SimpleDateFormat("d")
+            val yearMonth: String = sdf0.format(d)
+            val dayOfMonth: String = sdf1.format(d)
+            val LUNAR_MONTH = 29.530588853;
+            val newMoonDate = when(yearMonth){
+                "2022 January" -> 2
+                "2022 February" -> 1
+                "2022 March" -> 2
+                "2022 April" -> 1
+                "2022 May" -> 30
+                "2022 June" -> 28
+                "2022 July" -> 28
+                "2022 August" -> 27
+                "2022 September" -> 25
+                "2022 October" -> 25
+                "2022 November" -> 23
+                "2022 December" -> 23
+                "2023 January" -> 21
+                "2023 February" -> 19
+                "2023 March" -> 21
+                "2023 April" -> 19
+                "2023 May" -> 19
+                "2023 June" -> 17
+                "2023 July" -> 17
+                "2023 August" -> 16
+                "2023 September" -> 14
+                "2023 October" -> 14
+                "2023 November" -> 13
+                "2023 December" -> 12
+                else -> 1
+            }
+            val fullMoonDate = when(yearMonth){
+                "2022 January" -> 17
+                "2022 February" -> 16
+                "2022 March" -> 18
+                "2022 April" -> 16
+                "2022 May" -> 16
+                "2022 June" -> 14
+                "2022 July" -> 13
+                "2022 August" -> 11
+                "2022 September" -> 10
+                "2022 October" -> 9
+                "2022 November" -> 8
+                "2022 December" -> 7
+                "2023 January" -> 6
+                "2023 February" -> 5
+                "2023 March" -> 7
+                "2023 April" -> 5
+                "2023 May" -> 5
+                "2023 June" -> 3
+                "2023 July" -> 3
+                "2023 August" -> 1
+                "2023 September" -> 29
+                "2023 October" -> 28
+                "2023 November" -> 27
+                "2023 December" -> 26
+                else -> 1
+            }
 
-        val moonString = if((moonPercent * 100) in 0..10 || (moonPercent * 100) in 90..100){"Newmoon"}
-            else if (toInt(moonPercent * 100) in 45..54) {"Full Moon"}
-            else if((moonPercent * 100) in 10..24) {"Right Crescent Moon"}
-            else if((moonPercent * 100) in 75..89) {"Left Crescent Moon"}
-            else if((moonPercent * 100) in 35..44) {"Right Gib Moon"}
-            else if((moonPercent * 100) in 55..64) {"left Gib Moon"}
-        else if((moonPercent * 100) in 55..64) {"left Gib Moon"}
-            else {"half moon"}
-        //0-10% newmoon
-        //10 - 25% crescent right
-        //25%-35% halfmoon right
-        //35-45% gib moon right
-        // 45% to 55%  full moon
-        // 55% to 65%  gib left moon
-        //65% - 75% halfmoon left
-        //75-90% crescent left
-        //90-100% newmoon
-
-
-
-            //array
-            //January 31 2022
-            //March 2 2022
-            //April 30 2022
-            //May 30 2022
-            //June 28 2022
-            //July 28 2022
-            //August 27 2022
-            //September 25 2022
-            //October 25 2022
-            //November 23 2022
-            //December 23 2022
-
-
-            // Date date = sdf5.parse(arrayNewMoon());
-            //long diff = endDateValue.getTime() - startDateValue.getTime();
-            //TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
-
-            //Moon Phases Function
-            //moon phase length is roughly 29.5 days
-            //get number of days in year current date, subtract days from newmoon
-            //fraction less than 1 = newmoon 0-10% and 90%-100% newmoon
-            //0-10% newmoon
-            //10 - 25% crescent right
-            //25%-35% halfmoon right
-            //35-45% gib moon right
-            // 45% to 55%  full moon
-            // 55% to 65%  gib left moon
-            //65% - 75% halfmoon left
-            //75-90% crescent left
-            //90-100% newmoon
-
-            //daysSinceNewMoon  / 29.5 days
-        }
+            val newMoondifference = abs((Integer.parseInt(dayOfMonth)) - newMoonDate)
+            val moonPercent: Double = newMoondifference / LUNAR_MONTH
+            val moonString : String = if(moonPercent < 0.05 ){"New Moon"}
+            else if (moonPercent >= .05 && moonPercent < 0.25 ){"Waxing Crescent Moon"}
+            else if(moonPercent >=0.25 && moonPercent < 0.35){"Waxing Half Moon"}
+            else if(moonPercent >=0.35 && moonPercent < 0.47){"Waxing Gibbous Moon"}
+            else if(moonPercent >=0.47 && moonPercent < 0.55){"Full Moon"}
+            else if(moonPercent >=0.55 && moonPercent < 0.65){"Waning Gibbous Moon"}
+            else if(moonPercent >=0.65 && moonPercent < 0.75){"Waning half Moon"}
+            else if(moonPercent >=0.75 && moonPercent < 0.95){"Waning Crescent Moon"}
+            else {"New Moon"}
+            //0% - 5% new moon | USE : ARRAY EXACT DATE
+            //5% - 25% crescent right
+            //25% - 35% half moon right
+            //35 - 45% gib moon right
+            // 45% - 55%  full moon | USE : ARRAY EXACT DATE
+            // 55% - 65%  gib left moon
+            //65% - 75% half moon left
+            //75% - 90% crescent left
+            //90% - 100% new moon | USE : ARRAY EXACT DATE
             return moonString
-        }*/
+        }
